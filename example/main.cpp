85803a2f9f24815b979ca0a26a12fffb7b97f659
David Webb <david@dpwlabs.com>
2019-11-26 07:11:49 +1030

    Several Code Improvements
    * More const and noexcept
    * Correctly handle global ssl context - no global variable in header
    * Minor refactor - more consistent class layouts
    * Improvements url class
      * Better copy constructor and copy assignment
      * Implemented move constructor and move assignment
    * Added clang-format for code style consistency

--- example/main.cpp
+++ example/main.cpp
@@ -6,56 +6,61 @@
 #include <boost/asio.hpp>
 #include <iostream>
 
-int main(int argc, char *argv[]) {
-  std::cout << "Start up\n";
+int main(int /*argc*/, char* /*argv*/[])
+{
+    std::cout << "Start up\n";
 
-  try {
-    boost::asio::io_context ioContext;
+    try {
+        boost::asio::io_context ioContext;
 
-    // URL parsing validation tests - will throw exception if any URL is invalid.
-    simple_http::url test("http://test.com/target");
-    simple_http::url test2("www.test.com/target2");
-    simple_http::url test3("https://test.com");
-    simple_http::url test4("test.com:80");
-    simple_http::url test5("http://33.com:400/target");
-    simple_http::url test6("http://user:pass@33.com:400/target?val=1&val2=2");
-    simple_http::url test7("http://user:pass@33.com");
-    
-    
-    // Run an asynchronous client test - connect with Digest Authentication
-    {
-      auto client = std::make_shared<simple_http::get_client>(
-          ioContext, [](simple_http::empty_body_request &req,
-                        simple_http::string_body_response &resp) {
-            // Display the response to the console.
-            std::cout << resp << '\n';
-          });
-      // Run the GET request to httpbin.org
-      client->get(simple_http::url(
-          "http://user:passwd@httpbin.org/digest-auth/auth/user/passwd/MD5/never"));
+        // URL parsing validation tests - will throw exception if any URL is invalid.
+        simple_http::url test{"http://test.com/target"};
+        simple_http::url test2{"www.test.com/target2"};
+        simple_http::url test3{"https://test.com"};
+        simple_http::url test4{"test.com:80"};
+        simple_http::url test5{"http://33.com:400/target"};
+        simple_http::url test6{"http://user:pass@33.com:400/target?val=1&val2=2"};
+        simple_http::url test7{"http://user:pass@33.com"};
+
+        // Run an asynchronous client test - connect with Digest Authentication
+        {
+            auto client = std::make_shared<simple_http::get_client>(
+                ioContext,
+                [](simple_http::empty_body_request& /*req*/,
+                   simple_http::string_body_response& resp) {
+                    // Display the response to the console.
+                    std::cout << resp << '\n';
+                });
+
+            // Run the GET request to httpbin.org
+            client->get(simple_http::url{
+                "http://user:passwd@httpbin.org/digest-auth/auth/user/passwd/MD5/never"});
+        }
+
+        // Run another asynchronous client test - redirection and HTTPS connect.
+        // This example shows the boost::beast request and response template classes.
+        {
+            auto client = std::make_shared<simple_http::get_client>(
+                ioContext,
+                [](boost::beast::http::request<boost::beast::http::empty_body>& /*req*/,
+                   boost::beast::http::response<boost::beast::http::string_body>& resp) {
+                    std::cout << resp << '\n';
+                });
+            client->setFailHandler([](const simple_http::empty_body_request& /*req*/,
+                                      const simple_http::string_body_response& /*resp*/,
+                                      simple_http::fail_reason /*fr*/, boost::string_view message) {
+                std::cout << message << '\n';
+            });
+            // Connect to google.com - which enforces HTTPS, through a 301 response.
+            // The 1 argument is the number of redirects to follow.
+            client->get(simple_http::url{"http://google.com"}, 1);
+        }
+
+        // Run the until requests are complete.
+        ioContext.run();
+    } catch (std::exception& e) {
+        std::cerr << "exception: " << e.what() << "\n";
     }
 
-    // Run another asynchronous client test - redirection and HTTPS connect.
-    // This example shows the boost::beast request and response template classes.
-    {
-      auto client = std::make_shared<simple_http::get_client>(
-          ioContext, [](boost::beast::http::request<boost::beast::http::empty_body> &req,
-                        boost::beast::http::response<boost::beast::http::string_body> &resp) {
-            std::cout << resp << '\n';
-          });
-      client->setFailHandler([](simple_http::empty_body_request &req,
-                            simple_http::string_body_response &resp, simple_http::fail_reason fr, boost::string_view message) {
-        std::cout << message << '\n';
-      });
-      // Connect to www.dpwlabs.com - which enforces HTTPS, through a 301 response.
-      // The 1 argument is the number of redirects to follow.
-      client->get(simple_http::url("http://google.com"), 1);
-    }
-
-    // Run the until requests are complete.
-    ioContext.run();
-  } catch (std::exception &e) {
-    std::cerr << "exception: " << e.what() << "\n";
-  }
-  return 0;
+    return 0;
 }
--- include/httpclient.hpp
+++ include/httpclient.hpp
@@ -19,121 +19,126 @@ namespace simple_http {
  * @brief The simple_client class a client for HTTP
  */
 
-template <typename RequestBody, typename ResponseBody>
-class basic_client : public std::enable_shared_from_this<basic_client<RequestBody, ResponseBody>> {
+template<typename RequestBody, typename ResponseBody>
+class basic_client : public std::enable_shared_from_this<basic_client<RequestBody, ResponseBody>>
+{
 public:
-  explicit basic_client(boost::asio::io_context &io, int timeoutMs = 3000)
-      : m_io{io}, m_timeoutMs{timeoutMs}, m_state{Resolve} {
-  }
+    explicit constexpr basic_client(boost::asio::io_context& io, int timeoutMs = 3000) noexcept
+      : m_io{io}, m_timeoutMs{timeoutMs}, m_state{Resolve}
+    {}
 
-  explicit basic_client(boost::asio::io_context &io,
-                        std::function<void(boost::beast::http::request<RequestBody> &,
-                                           boost::beast::http::response<ResponseBody> &)>
-                            responseHandler,
-                        int timeoutMs = 3000)
-      : m_io{io}, m_responseHandler{responseHandler}, m_timeoutMs{timeoutMs} {
-  }
+    explicit basic_client(boost::asio::io_context& io,
+                          std::function<void(boost::beast::http::request<RequestBody>&,
+                                             boost::beast::http::response<ResponseBody>&)>
+                              responseHandler,
+                          int timeoutMs = 3000)
+      : m_io{io}, m_responseHandler{responseHandler}, m_timeoutMs{timeoutMs}
+    {}
 
-  void get(const url &uri, int maxRedirects = 0, bool basicAuth = false, int version = 11) {
-    performRequest(uri, boost::beast::http::verb::get, typename RequestBody::value_type{},
-                   maxRedirects, basicAuth, version);
-  }
-
-  void post(const url &uri, typename RequestBody::value_type requestBody, int maxRedirects = 0,
-            bool basicAuth = false, int version = 11) {
-    performRequest(uri, boost::beast::http::verb::post, requestBody, maxRedirects, basicAuth,
-                   version);
-  }
-
-  void performRequest(const url &uri, boost::beast::http::verb method, int maxRedirects = 0,
-                      bool basicAuth = false, int version = 11) {
-    performRequest(uri, method, typename RequestBody::value_type{}, maxRedirects, basicAuth,
-                   version);
-  }
-
-  void performRequest(const url &uri, boost::beast::http::verb method,
-                      typename RequestBody::value_type requestBody, int maxRedirects = 0,
-                      bool basicAuth = false, int version = 11) {
-    m_url = uri;
-    if (m_url.hasAuthentication() && (m_username.empty() || m_password.empty())) {
-      setAuthorization(m_url.username(), m_url.password(), basicAuth);
+    void get(url&& uri, int maxRedirects = 0, bool basicAuth = false, int version = 11)
+    {
+        performRequest(std::forward<url>(uri), boost::beast::http::verb::get,
+                       typename RequestBody::value_type{}, maxRedirects, basicAuth, version);
     }
-    std::shared_ptr<client_private<RequestBody, ResponseBody>> httpclient =
-        client_private<RequestBody, ResponseBody>::privateForRequest(m_url,
-                                                                     this->shared_from_this());
-    if (httpclient) {
-      httpclient->performRequest(uri, method, requestBody, maxRedirects, basicAuth, version);
-      m_p = httpclient;
+
+    void post(url&& uri, typename RequestBody::value_type requestBody, int maxRedirects = 0,
+              bool basicAuth = false, int version = 11)
+    {
+        performRequest(uri, boost::beast::http::verb::post, requestBody, maxRedirects, basicAuth,
+                       version);
     }
-  }
 
-  void setAuthorization(boost::string_view username, boost::string_view password,
-                        bool forceBasic = false) {
-    m_basicAuthForce = forceBasic;
-    m_username = username.to_string();
-    m_password = password.to_string();
-  }
-
-  inline void abort() {
-    if (auto cl = m_p.lock()) {
-      cl->abort();
+    void performRequest(url&& uri, boost::beast::http::verb method, int maxRedirects = 0,
+                        bool basicAuth = false, int version = 11)
+    {
+        performRequest(std::forward<url>(uri), method, typename RequestBody::value_type{},
+                       maxRedirects, basicAuth, version);
     }
-  }
 
-  void setResponseHandler(std::function<void(boost::beast::http::request<RequestBody> &,
-                                             boost::beast::http::response<ResponseBody> &)>
-                              responseHandler) {
-    m_responseHandler = responseHandler;
-  }
+    void performRequest(url&& uri, boost::beast::http::verb method,
+                        typename RequestBody::value_type requestBody, int maxRedirects = 0,
+                        bool basicAuth = false, int version = 11)
+    {
+        if (uri.hasAuthentication() && (m_username.empty() || m_password.empty())) {
+            setAuthorization(uri.username(), uri.password(), basicAuth);
+        }
+        std::shared_ptr<client_private<RequestBody, ResponseBody>> httpclient =
+            client_private<RequestBody, ResponseBody>::privateForRequest(uri,
+                                                                         this->shared_from_this());
+        if (httpclient) {
+            httpclient->performRequest(std::forward<url>(uri), method, requestBody, maxRedirects,
+                                       basicAuth, version);
+            m_p = httpclient;
+        }
+    }
 
-  void setFailHandler(std::function<void(boost::beast::http::request<RequestBody> &,
-                                         boost::beast::http::response<ResponseBody> &, fail_reason,
-                                         boost::string_view)>
-                          failHandler) {
-    m_failHandler = failHandler;
-  }
+    void setAuthorization(boost::string_view username, boost::string_view password,
+                          bool forceBasic = false) noexcept
+    {
+        m_basicAuthForce = forceBasic;
+        m_username = username.to_string();
+        m_password = password.to_string();
+    }
 
-  client_state state() const {
-    return m_state;
-  }
+    void abort()
+    {
+        if (auto cl = m_p.lock()) {
+            cl->abort();
+        }
+    }
+
+    void setResponseHandler(std::function<void(boost::beast::http::request<RequestBody>&,
+                                               boost::beast::http::response<ResponseBody>&)>
+                                responseHandler) noexcept
+    {
+        m_responseHandler = responseHandler;
+    }
+
+    void setFailHandler(std::function<void(const boost::beast::http::request<RequestBody>&,
+                                           const boost::beast::http::response<ResponseBody>&, fail_reason,
+                                           boost::string_view)>
+                            failHandler) noexcept
+    {
+        m_failHandler = failHandler;
+    }
+
+    client_state state() const noexcept { return m_state; }
 
 private:
-  boost::asio::io_context &m_io;
-  url m_url;
-  std::string m_username;
-  std::string m_password;
-  int m_maxRedirects;
-  int m_timeoutMs;
-  bool m_basicAuthForce;
-  std::function<void(boost::beast::http::request<RequestBody> &,
-                     boost::beast::http::response<ResponseBody> &)>
-      m_responseHandler;
-  std::function<void(boost::beast::http::request<RequestBody> &,
-                     boost::beast::http::response<ResponseBody> &, fail_reason, boost::string_view)>
-      m_failHandler;
-  client_state m_state;
+    boost::asio::io_context& m_io;
+    std::string m_username;
+    std::string m_password;
+    int m_maxRedirects;
+    int m_timeoutMs;
+    bool m_basicAuthForce;
 
-  boost::string_view userAgent() {
-    return "simple-beast-client/1.0";
-  }
+    std::function<void(boost::beast::http::request<RequestBody>&,
+                       boost::beast::http::response<ResponseBody>&)>
+        m_responseHandler;
+    std::function<void(const boost::beast::http::request<RequestBody>&,
+                       const boost::beast::http::response<ResponseBody>&, fail_reason,
+                       boost::string_view)>
+        m_failHandler;
+    client_state m_state;
 
-  void failure(fail_reason reason, boost::string_view message) {
-    static boost::beast::http::request<RequestBody> req;
-    static boost::beast::http::response<ResponseBody> resp;
-    try {
-      m_failHandler(req, resp, reason, message);
-    } catch (std::bad_function_call & /*f*/) {
-      // Discard silently.
+    boost::string_view userAgent() const noexcept { return "simple-beast-client/1.1"; }
+
+    void failure(fail_reason reason, boost::string_view message)
+    {
+        const boost::beast::http::request<RequestBody> req{};
+        const boost::beast::http::response<ResponseBody> resp{};
+        try {
+            m_failHandler(req, resp, reason, message);
+        } catch (std::bad_function_call& /*f*/) {
+            // Discard silently.
+        }
     }
-  }
 
-  friend class client_private<RequestBody, ResponseBody>;
 
-  std::weak_ptr<client_private<RequestBody, ResponseBody>> m_p;
-  friend class client_private_http<RequestBody, ResponseBody>;
-#ifdef ENABLE_HTTPS
-  friend class client_private_ssl<RequestBody, ResponseBody>;
-#endif
+    std::weak_ptr<client_private<RequestBody, ResponseBody>> m_p;
+
+
+    friend class client_private<RequestBody, ResponseBody>;
 };
 
 typedef boost::beast::http::request<boost::beast::http::empty_body> empty_body_request;
